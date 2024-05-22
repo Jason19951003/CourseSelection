65aa1f525d3c9b31b7c5fd04b4a4fa4fb262aa98
Jason19951003 <Jason19951003@gmail.com>
2024-05-22 15:10:40 +0800

    新增 insertCourse 方法

--- src/main/java/course/selection/dao/CourseMapper.java
+++ src/main/java/course/selection/dao/CourseMapper.java
@@ -5,4 +5,6 @@ import java.util.Map;
 
 public interface CourseMapper {
     public List<Map<String, Object>> findCourse(Map<String, Object> param);
+
+    public Integer insertCourse(Map<String, Object> param);
 }
