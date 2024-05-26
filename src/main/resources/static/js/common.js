102521e267d26264e154d0363d22e33fd867d17b
Jason19951003 <Jason19951003@gmail.com>
2024-05-26 19:21:46 +0800

    修改前端架構

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,26 +1,26 @@
 const loadDepartment = async() => {
-    const response = await fetch('http://localhost:8080/findDepartment');
+    const response = await fetch('http://localhost:8080/course/findDepartment');
     const {state, message, data} = await response.json();
     
     data.forEach(obj => {
-        $('#courseDep').append(`<option value="${obj.departmentId}">${obj.departmentName}</option>`);
+        $('#courseDep').append(`<option value="${obj.departmentId}">${obj.departmentName}</>`);
     });
+
+    $('#courseDep').on('change', async() => {
+        // 要先清空Select裡的html才能append(不然會接續在後面)
+        $('#teacherId').html('');
+        const courseDep = $('#courseDep').val();
+        const response = await fetch(`http://localhost:8080/course/findTeacher/${courseDep}`);
+        const {state, message, data} = await response.json();
+        data.forEach(obj => {
+            $('#teacherId').append(`<option value="${obj.userId}">${obj.userName}</option>`)
+        });
+    });
+
     $('#courseDep').change();
 }
 
-$('#courseDep').on('change', async() => {
-    // 要先清空Select裡的html才能append(不然會接續在後面)
-    $('#teacherId').html('');
-    const courseDep = $('#courseDep').val();
-    const response = await fetch(`http://localhost:8080/findTeacher/${courseDep}`);
-    const {state, message, data} = await response.json();
-    data.forEach(obj => {
-        $('#teacherId').append(`<option value="${obj.userId}">${obj.userName}</option>`)
-    })
-});
-
-
-const renderHtml = async(id, url) => {
+const renderHtml = async(id, url) => {    
     const response = await fetch(`http://localhost:8080/${url}`);
     const html = await response.text();
     $(`#${id}`).html(html);
