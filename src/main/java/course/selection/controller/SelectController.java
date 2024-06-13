28db1ba02067197c17900ef3161c0db6a72a2b87
Jason19951003 <Jason19951003@gmail.com>
2024-06-13 20:18:49 +0800

    test

--- src/main/java/course/selection/controller/SelectController.java
+++ src/main/java/course/selection/controller/SelectController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import course.selection.model.ApiResponse;
 import course.selection.service.SelectService;
