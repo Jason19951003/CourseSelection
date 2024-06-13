4c6af6fe1dcd2a2b5f075e259fbeb5f0ec2e298d
Jason19951003 <Jason19951003@gmail.com>
2024-06-13 22:42:56 +0800

    status初始化

--- src/main/java/course/selection/config/InitCourseStatus.java
+++ src/main/java/course/selection/config/InitCourseStatus.java
@@ -2,6 +2,7 @@ package course.selection.config;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,10 +20,19 @@ public class InitCourseStatus {
     @Bean
     public List<CourseScore> getStatus() {
         List<Map<String, Object>> listMap = selectMapper.findCourseCapacity();
-        List<CourseScore> status = selectMapper.checkCourseStatus();
-        for (Map<String, Object> map : listMap) {
-            
-        }
+		List<CourseScore> status = selectMapper.checkCourseStatus();
+		for (Map<String, Object> map : listMap) {
+			Optional<CourseScore> result = status.stream()
+					.filter(CourseScore -> CourseScore.getCourseDep().equals(map.get("course_dep"))
+							&& CourseScore.getCourseId().equals(map.get("course_id")))
+					.findFirst();
+			// 處理找到的結果
+			if (result.isPresent()) {
+				CourseScore courseScore = result.get();
+				courseScore.setCourseCapacity(
+						courseScore.getCourseCapacity() - Integer.parseInt(map.get("capacity") + ""));
+			}
+		}
         return status;
     }
 }
