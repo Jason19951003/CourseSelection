7363ac6aaa9946fbeb77ba7034d7a80b418eaf16
Jason19951003 <Jason19951003@gmail.com>
2024-06-14 16:35:47 +0800

    分頁

--- src/main/resources/static/js/root_user.js
+++ src/main/resources/static/js/root_user.js
@@ -139,5 +139,13 @@ const searchUser = async(body, modal, permissionId) => {
                 </tr>
             `)
         });
+        new DataTable('#studentTable', {
+            searching: false,
+            initComplete: function () {
+                var pagination = $('#studentTable_paginate').detach();
+                console.log(pagination);
+                $('#mypagination').append(pagination);
+            }
+        });
     }
 }
