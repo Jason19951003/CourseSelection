b6666486d6c08b522948007b8a8ee73ea3f5ccca
Jason19951003 <NTPU@host.docker.internal>
2024-06-19 17:06:27 +0800

    schedule

---
+++ src/main/java/course/selection/dao/ScheduleMapper.java
@@ -0,0 +1,11 @@
+package course.selection.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ScheduleMapper {
+
+    public List<Map<String, Object>> findAllCourseInfo();
+
+    public List<Map<String, Object>> findAllClassInfo();
+}
